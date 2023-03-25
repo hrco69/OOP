549d7ad3f9c4974f2fe7f11bee56740e71a72407
NotHrco <78282321+hrco69@users.noreply.github.com>
2023-03-25 12:03:52 +0100

    Add files via upload

---
+++ default.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <string>
+#include <ctime>
+#define cahr char
+#define enld endl
+using namespace std;
+
+class {
+public:
+	
+};
+
+int main() {
+
+	return 0;
+}
